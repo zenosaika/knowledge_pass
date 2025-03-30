d6954d29ce22c5cc3107cde016a04c8c59d792ee
zenosaika <zenosaika@gmail.com>
2025-03-30 20:34:54 +0700

    update README.md for v1.1.0

--- KPFrontend/AdminDashboard/views.py
+++ KPFrontend/AdminDashboard/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def admin_dashboard(req):
     return render(req, 'AdminDashboard/new_admin_dashboard.html.html')
--- KPFrontend/KnowledgePass/settings.py
+++ KPFrontend/KnowledgePass/settings.py
@@ -124,6 +124,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",  # Ensure this points to your static folder
 ]
 
+LOGIN_URL = '/admin/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
