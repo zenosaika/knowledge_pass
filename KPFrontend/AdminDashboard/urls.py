853818962e0d22c91837ba524befafdddc4dc973
zenosaika <zenosaika@gmail.com>
2025-03-30 20:05:21 +0700

    enable route to admin dashboard

--- KPFrontend/AdminDashboard/urls.py
+++ KPFrontend/AdminDashboard/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin_dashboard', views.admin_dashboard, name='admin_dashboard'),
+    path('admin_dashboard/', views.admin_dashboard, name='admin_dashboard'),
 ]
--- KPFrontend/AdminDashboard/views.py
+++ KPFrontend/AdminDashboard/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def admin_dashboard(req):
-    return render(req, 'AdminDashboard/admin_dashboard.html')
+    return render(req, 'AdminDashboard/new_admin_dashboard.html.html')
